da3a210d78e661b00b212a0edb8eff1c22677053
Pieter Eendebak <pieter.eendebak@gmail.com>
2016-11-17 17:47:46 +0100

    add timediff

--- qcodes/instrument_drivers/QuTech/TimeStamp.py
+++ qcodes/instrument_drivers/QuTech/TimeStamp.py
@@ -17,12 +17,20 @@ class TimeStampInstrument(Instrument):
     '''
     def __init__(self, name, **kwargs):
         super().__init__(name, **kwargs)
+        self.add_parameter('timediff', units='s', get_cmd=self.get_time_diff, docstring='Timestamp based on number of seconds since epoch.')
         self.add_parameter('timestamp', units='s', get_cmd=time.time, docstring='Timestamp based on number of seconds since epoch.')
         self.add_parameter('timestring', get_cmd=self.get_timestring, docstring='Time string based on number of seconds since epoch.')
         
+        self.set_time_offset()
         _ = self.timestamp.get()
         _ = self.timestring.get()
-        
+
+    def set_time_offset(self, t0=None):
+        if t0 is None:
+            t0 = time.time()
+        self.t0 = t0
+    def get_time_diff(self):
+        return time.time()-self.t0        
 
     def get_timestring(self):
         """ Return string representation of current time """
